7b1f6a56047f5800bd9a51fd0e4a7517b810118b
Lucien <lokm13@gmail.com>
2019-04-30 11:03:32 +0000

    drafted the noise in baseline

--- baseline.py
+++ baseline.py
@@ -72,7 +72,7 @@ audios = []
 audio_lengths = []
 # project_eps = 10 ** (75/20.)
 project_eps = 2303.350347178
-read_unipertur = np.array(wav.read("./audios/unipertur.wav"))
+read_unipertur = np.array(wav.read("./audios/example.wav"))
 
 if args.out is None:
     assert args.outprefix is not None
@@ -128,7 +128,8 @@ tfrescale = tf.Variable(np.zeros((batch_size,1), dtype=np.float32), name='qq_phr
 tfimportance = tf.Variable(np.zeros((batch_size, phrase_length), dtype=np.float32), name='qq_importance')
 
 unipertur = np.zeros((batch_size, max_audio_len), dtype=np.float32)
-unipertur += np.array(read_unipertur)
+unipertur = np.clip(np.random.normal(0, project_eps, size=unipertur.shape), -project_eps, -project_eps)
+unipertur[:read_unipertur.shape[0]] = read_unipertur
 
 # Initially we bound the l_infty norm by 2000, increase this
 # constant if it's not big enough of a distortion for your dataset.
@@ -207,9 +208,8 @@ if finetune is not None and len(finetune) > 0:
 # We'll make a bunch of iterations of gradient descent here
 now = time.time()
 n_fooled_test = 0
-shuffled_indices = list(range(len(audios)))
-random.shuffle(shuffled_indices)
-for idx_audio in shuffled_indices:
+audio_indices = list(range(len(audios)))
+for idx_audio in audio_indices:
     print("=" * 40)
     print("Training for audio: %d"%idx_audio)
     audio = audios[idx_audio]
@@ -227,90 +227,45 @@ for idx_audio in shuffled_indices:
     sess.run(tfimportance.assign(c))
     sess.run(tfrescale.assign(np.ones((batch_size,1))))
 
-    for i in range(args.each_train):
-        iteration = i
-        now = time.time()
+    new, delta, r_out, r_logits = sess.run((tfnew_input, tfdelta, tfdecoded, tflogits))
+    lst = [(r_out, r_logits)]
 
-        # Print out some debug information every 10 iterations.
-        if i%10 == 0:
-            new, delta, r_out, r_logits = sess.run((tfnew_input, tfdelta, tfdecoded, tflogits))
-            lst = [(r_out, r_logits)]
+    for out, logits in lst:
+        chars = out[0].values
 
-            for out, logits in lst:
-                chars = out[0].values
+        res = np.zeros(out[0].dense_shape)+len(toks)-1
+    
+        for ii in range(len(out[0].values)):
+            x,y = out[0].indices[ii]
+            res[x,y] = out[0].values[ii]
 
-                res = np.zeros(out[0].dense_shape)+len(toks)-1
-            
-                for ii in range(len(out[0].values)):
-                    x,y = out[0].indices[ii]
-                    res[x,y] = out[0].values[ii]
+        # Here we print the strings that are recognized.
+        res = ["".join(toks[int(x)] for x in y).replace("-","") for y in res] 
+        if i == 0 and res[0] == args.target:
+            if idx_audio < args.n_train:
+                n_fooled_train += 1
+            else:
+                n_fooled_test += 1
+        print("\n".join(res)) 
+        # And here we print the argmax of the alignment.
+        res2 = np.argmax(logits,axis=2).T
+        res2 = ["".join(toks[int(x)] for x in y[:(l-1)//320]) for y,l in zip(res2,audio_lengths)]
+        print("\n".join(res2))
 
-                # Here we print the strings that are recognized.
-                res = ["".join(toks[int(x)] for x in y).replace("-","") for y in res] 
-                if i == 0 and res[0] == args.target:
-                    if idx_audio < args.n_train:
-                        n_fooled_train += 1
-                    else:
-                        n_fooled_test += 1
-                print("\n".join(res)) 
-                # And here we print the argmax of the alignment.
-                res2 = np.argmax(logits,axis=2).T
-                res2 = ["".join(toks[int(x)] for x in y[:(l-1)//320]) for y,l in zip(res2,audio_lengths)]
-                print("\n".join(res2))
-
-        feed_dict = {}
-        # Minimize delta
-        # Actually do the optimization ste
-        d, el, cl, l, logits, new_input, _ = sess.run((tfdelta, tfexpanded_loss,
-                                                        tfctcloss, tfloss,
-                                                        tflogits, tfnew_input,
-                                                        train),
-                                                        feed_dict)
-        # Report progress
-        cur_loss = np.mean(cl)
-        if i % 10 == 0:
-            print("%.3f"%np.mean(cl), "\t", "\t".join("%.3f"%x for x in cl))
-        if idx_audio >= args.n_train:
-            print("It was TEST")
-            break
-
-        logits = np.argmax(logits,axis=2).T
-        for ii in range(batch_size):
-            # Every 100 iterations, check if we've succeeded
-            # if we have (or if it's the final epoch) then we
-            # should record our progress and decrease the
-            # rescale constant.
-            if (loss_fn == "CTC" and i == 0 and res[ii] == "".join([toks[x] for x in target[ii]])) \
-                or (i == MAX-1 and final_deltas[ii] is None):
-                # Get the current constant
-                rescale = sess.run(tfrescale)
-                if rescale[ii]*2000 > np.max(np.abs(d)):
-                    # If we're already below the threshold, then
-                    # just reduce the threshold to the current
-                    # point and save some time.
-                    print("It's way over", np.max(np.abs(d[ii]))/2000.0)
-                    rescale[ii] = np.max(np.abs(d[ii]))/2000.0
-
-                # Otherwise reduce it by some constant. The closer
-                # this number is to 1, the better quality the result
-                # will be. The smaller, the quicker we'll converge
-                # on a result but it will be lower quality.
-                rescale[ii] *= .8
-
-                # Adjust the best solution found so far
-                final_deltas[ii] = new_input[ii]
-
-                print("Worked i=%d ctcloss=%f bound=%f"%(ii,cl[ii], 2000*rescale[ii][0]))
-                #print('delta',np.max(np.abs(new_input[ii]-audio[ii])))
-                sess.run(tfrescale.assign(rescale))
-
-                # Just for debugging, save the adversarial example
-                # to /tmp so we can see it if we want
-                wav.write("./audios/adv%04d.wav"%idx_audio, 16000,
-                            np.array(np.clip(np.round(new_input[ii]), -2**15, 2**15-1),dtype=np.int16))
-
-        if cur_loss < 1:
-            break
+    feed_dict = {}
+    # Minimize delta
+    # Actually do the optimization ste
+    d, el, cl, l, logits, new_input, _ = sess.run((tfdelta, tfexpanded_loss,
+                                                    tfctcloss, tfloss,
+                                                    tflogits, tfnew_input,
+                                                    train),
+                                                    feed_dict)
+    # Report progress
+    cur_loss = np.mean(cl)
+    print("%.3f"%np.mean(cl), "\t", "\t".join("%.3f"%x for x in cl))
+    if idx_audio >= args.n_train:
+        print("It was TEST")
+        break
 
     if idx_audio < args.n_train:
         print("delta L2:", np.mean(np.square(d)))
@@ -320,9 +275,5 @@ for idx_audio in shuffled_indices:
 fool_rate_train = float(n_fooled_train) / args.n_train
 fool_rate_test = float(n_fooled_test) / args.n_test
 print("training fooling rate: %f, testing fooling rate: %f, project_eps: %f"%(fool_rate_train, fool_rate_test, project_eps))
-if fool_rate_test > 0.75:
-    # project_eps /= float(10 ** (5/20))
-    project_eps *= 0.8
-    wav.write("./audios/unipertur.wav", 16000, np.array(np.clip(np.round(unipertur[0]), -2**15, 2**15-1),dtype=np.int16))
 
 sess.close()
